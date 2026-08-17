87d57aa0963f86815c6c32b73d5991e0489b591f
cdbcnk8p5p-cyber <cdbcnk8p5p@privaterelay.appleid.com>
2026-08-17 23:49:43 +0100

    Add shelf-selector service worker cache version

---
+++ sw-shelf-tabs.js
@@ -0,0 +1,42 @@
+const CACHE='game-museum-v3-5-3-shelf-tabs-v1';
+const CORE=[
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './data.js',
+  './platform-standard.js?v=1',
+  './haul-update-2026-08-12.js?v=2',
+  './nintendo-ds-update.js',
+  './nintendo-sega-update.js?v=1',
+  './ps2-update.js?v=1',
+  './wishlist-covers.js?v=2',
+  './data-integrity.js?v=1',
+  './filter-order.js?v=3',
+  './collection-platform-filter.js?v=2',
+  './wishlist-gallery.js?v=1',
+  './wishlist-gallery.css?v=1',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(Promise.all([
+    self.clients.claim(),
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  ]));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request, {cache:'no-store'}).then(response => {
+      const copy=response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request,copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(response => response || caches.match('./index.html')))
+  );
+});
