9a38df5e074e4ebb9f508245026061614f64de75
cdbcnk8p5p-cyber <cdbcnk8p5p@privaterelay.appleid.com>
2026-08-20 21:51:56 +0100

    Bump cache for Collection Shelf naming and PS1 Platinum

--- sw-shelf-tabs.js
+++ sw-shelf-tabs.js
@@ -1,4 +1,4 @@
-const CACHE='game-museum-v3-5-3-shelf-tabs-v7';
+const CACHE='game-museum-v3-5-3-shelf-tabs-v8';
 const CORE=[
   './',
   './index.html',
@@ -16,6 +16,7 @@ const CORE=[
   './filter-order.js?v=3',
   './collection-platform-filter.js?v=2',
   './shelf-sections.js?v=1',
+  './collection-shelf-naming.js?v=1',
   './wishlist-gallery.js?v=1',
   './wishlist-gallery.css?v=1',
   './manifest.webmanifest',
