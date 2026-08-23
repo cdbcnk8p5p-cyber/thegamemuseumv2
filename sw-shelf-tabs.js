4393bb3097a2ddb8c295a73060acf9e8f8a82730
cdbcnk8p5p-cyber <cdbcnk8p5p@privaterelay.appleid.com>
2026-08-23 23:18:07 +0100

    Bump active cache for cool home hero tune

--- sw-shelf-tabs.js
+++ sw-shelf-tabs.js
@@ -1,9 +1,9 @@
-const CACHE='game-museum-v3-5-3-shelf-tabs-v34';
+const CACHE='game-museum-v3-5-3-shelf-tabs-v35';
 const CORE=[
   './',
   './index.html',
   './styles.css?v=2',
-  './theme-palette.css?v=1',
+  './theme-palette.css?v=2',
   './drawer-layer-fix.css?v=1',
   './app.js',
   './data.js',
